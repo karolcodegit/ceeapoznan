bd929d2a707cebb0e7fc0974023c8dc0332a4c20
Karol Znojkiewicz <karol.znojkiewicz@outlook.com>
2024-10-21 09:10:18 +0200

    add year in link

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -54,7 +54,7 @@ const MainLayout = ({ children }) => {
               <div className="flex flex-col items-end justify-end relative">
                 <button
                   onClick={togglePanel}
-                  className="fixed top-36 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-50"
+                  className="fixed top-60 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-50"
                   aria-label="Open modal"
                 >
                   <Bars4Icon className="h-6 w-6" />
