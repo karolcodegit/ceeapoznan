1d078a4d84d99788829525e3e7913406a009948f
Karol Znojkiewicz <karol.znojkiewicz@outlook.com>
2025-01-22 12:05:41 +0100

    fix problem function

--- netlify/functions/getCourse.js
+++ netlify/functions/getCourse.js
@@ -1,46 +1,58 @@
+const fetch = require("node-fetch");
+
 exports.handler = async function (event, context) {
-    const { request } = await import("graphql-request");
-    const endpoint = "https://graphql.datocms.com";
-    const query = `
-      query {
-          allCourses {
-              nameCourse
-              courseDuration
-              courseCost
-              date
-              description
-          }
+  const endpoint = "https://graphql.datocms.com";
+  const query = `
+    query {
+       allCourses(filter: { available: { eq: true } }) {
+        available
+        nameCourse
+        courseDuration
+        courseCost
+        date
+        description
+        detailedInformationAboutTheHotel{
+          conditionsOfParticipation
+          extras
+        }
       }
-    `;
-  
-    try {
-      console.log("Sending request to DatoCMS...");
-      console.log("Token:", process.env.DATOCMS_API_READONLY);
-      const data = await request(endpoint, query, {
-        headers: {
-          'Authorization': `Bearer ${process.env.DATOCMS_API_READONLY}`,
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'X-Api-Version': '3'
-        },
-      });
-      console.log("Response from DatoCMS:", data);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-      };
-    } catch (error) {
-      console.error(
-        "Request failed. Error details:",
-        error.response || error.message
-      );
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          error: "Something went wrong",
-          details: error.response || error.message,
-        }),
-      };
     }
-  };
-  
+  `;
+
+  try {
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.GATSBY_DATOCMS_API_READONLY}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const buffer = await response.arrayBuffer();
+    const text = new TextDecoder("utf-8").decode(buffer); // Dekodowanie UTF-8
+    const data = JSON.parse(text);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data, null, 2),
+    };
+  } catch (error) {
+    console.error("Request failed. Error details:", error.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Something went wrong",
+        details: error.message,
+      }),
+    };
+  }
+};
