b28024e424ea2638f50f3ae5222a3d3eb8e605ed
Karol Znojkiewicz <karol.znojkiewicz@outlook.com>
2025-01-24 16:32:20 +0100

    add bot assistent

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+export const onRenderBody = ({ setHtmlAttributes, setHeadComponents, setPostBodyComponents }) => {
   setHtmlAttributes({ lang: `pl` });
 
   setHeadComponents([
@@ -13,6 +13,30 @@ export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
       key="inpost-geowidget-js"
       src="https://geowidget.inpost.pl/inpost-geowidget.js"
       defer
+    />,
+  ]);
+
+  setPostBodyComponents([
+    <script
+      key="voiceflow-widget"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(d, t) {
+              var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+              v.onload = function() {
+                window.voiceflow.chat.load({
+                  verify: { projectID: '678cdd04f16cd4b7fb453161' },
+                  url: 'https://general-runtime.voiceflow.com',
+                  versionID: 'production'
+                });
+              }
+              v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
+              v.type = "text/javascript"; 
+              s.parentNode.insertBefore(v, s);
+          })(document, 'script');
+        `,
+      }}
     />,
   ]);
 };
