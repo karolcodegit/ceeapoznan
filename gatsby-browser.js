d887444bc06f5b81917b00c7e5ff111b6b4f2998
Karol Znojkiewicz <karol.znojkiewicz@outlook.com>
2026-08-25 13:17:52 +0200

    New pictures and added track to ceea.org.pl

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React from "react"
+import { Provider } from "react-redux"
 // import * as Sentry from "@sentry/react";
 // import { BrowserTracing } from "@sentry/react";
-import {store}  from './src/store/index';
-import './src/styles/global.css'
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import { store } from "./src/store/index"
+import "./src/styles/global.css"
+import "react-toastify/dist/ReactToastify.css"
 
 export const onClientEntry = () => {
-    if (typeof window !== 'undefined') {
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.href = 'https://geowidget.inpost.pl/inpost-geowidget.css';
-      document.head.appendChild(link);
-  
-      const script = document.createElement('script');
-      script.src = 'https://geowidget.inpost.pl/inpost-geowidget.js';
-      script.defer = true;
-      document.body.appendChild(script);
-    }
-  };
-  
-  // Sentry.init({
-  //   dsn: "https://ea0a7a50b85a8028c4a4fd11dac4f1d9@o4509803771265024.ingest.de.sentry.io/4509803783323728",
-  //   integrations: [new BrowserTracing()],
-  //   tracesSampleRate: 1.0, // Ustaw na 1.0, aby śledzić 100% transakcji (zmniejsz w produkcji)
-  //   sendDefaultPii: true,
-  //   debug: true, // Włącz debugowanie, aby zobaczyć logi w konsoli
-  //   environment: "development",
-  // });
+  if (typeof window !== "undefined") {
+    const link = document.createElement("link")
+    link.rel = "stylesheet"
+    link.href = "https://geowidget.inpost.pl/inpost-geowidget.css"
+    document.head.appendChild(link)
 
+    const script = document.createElement("script")
+    script.src = "https://geowidget.inpost.pl/inpost-geowidget.js"
+    script.defer = true
+    document.body.appendChild(script)
+  }
+}
+
+const track = path => {
+  try {
+    const data = JSON.stringify({ path, referrer: document.referrer || null })
+    navigator.sendBeacon(
+      "https://panel.ceea.org.pl/api/track",
+      new Blob([data], { type: "text/plain" })
+    )
+  } catch {}
+}
+
+exports.onInitialClientRender = () => {
+  track(window.location.pathname + window.location.search)
+}
+
+exports.onRouteUpdate = ({ location }) => {
+  track(location.pathname + location.search)
+}
 
 export const wrapRootElement = ({ element }) => (
   <Provider store={store}>{element}</Provider>
-);
+)
 
 //   export const onRouteUpdate = ({ location }) => {
 //     const params = new URLSearchParams(location.search);
